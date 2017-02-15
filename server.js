301ae31ca294a5906a526c776c3a9e4ceb43d879
george dimov <george dimov>
2017-02-15 18:10:16 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var app = express();
+var path = require('path')
+var bodyParser = require('body-parser')
+
+
+var http = require('http').Server(app);
+
+
+
+var port = 3000; 
+
+  
+
+app.set('views',path.join(__dirname,"/views"));
+app.set('view engine','ejs');
+app.engine('html',require ('ejs').renderFile);
+
+
+app.use(express.static(path.join(__dirname,'/relay')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var server = app.listen(port,()=>{
+    console.log('server started on port '+ port);
+})
+//socket-io
+var io = require('socket.io').listen(server);
+module.exports = {
+    io: io , 
+    http : http , 
+};
+var ioTimer = require('./timer/timer');
+var index = require('./routers/index');
+app.use('/',index.router);     
